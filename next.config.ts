dca10d5dde569f95408989ffa41a7caeed4b0ecd
Ahmed Malek <ahmedmalik1w@gmail.com>
2024-11-02 13:04:53 +0200

    debug vercel #8

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/",
         headers: [
           {
             key: "Content-Security-Policy",
